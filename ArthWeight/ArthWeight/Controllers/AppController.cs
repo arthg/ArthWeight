d9f640e95b0e78bf6ada460ec02242ee4756dbd5
Arthur Gorr <art.gorr@gmail.com>
2017-11-07 13:09:02 -0600

    Bunch of progress in prototype

---
+++ ArthWeight/ArthWeight/Controllers/AppController.cs
@@ -0,0 +1,29 @@
+﻿using ArthWeight.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+  public class AppController : Controller
+  {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(WeightViewModel weightViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.UserMessage = "Weight added";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArthWeight/ArthWeight/ViewModels/WeightViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArthWeight.ViewModels
+{
+    public class WeightViewModel
+    {
+        [Required]
+        public decimal Weight { get; set; }
+    }
+}
