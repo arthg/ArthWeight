d9939dc45fda4922892715f5cb8a984545617050
Arthur Gorr <art.gorr@gmail.com>
2017-11-09 16:54:45 -0600

    Logging

--- ArthWeight/ArthWeight/Data/ArthwindsRepository.cs
+++ ArthWeight/ArthWeight/Data/ArthwindsRepository.cs
@@ -1,4 +1,6 @@
 ﻿using ArthWeight.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,15 +9,26 @@ namespace ArthWeight.Data
     public class ArthwindsRepository : IArthwindsRepository
     {
         private readonly ArthwindsContext _arthwindsContext;
+        private readonly ILogger<ArthwindsRepository> _logger;
 
-        public ArthwindsRepository(ArthwindsContext arthwindsContext)
+        public ArthwindsRepository(ArthwindsContext arthwindsContext, 
+            ILogger<ArthwindsRepository> logger)
         {
             _arthwindsContext = arthwindsContext;
+            _logger = logger;
         }
 
         public IEnumerable<WeightEntry> WeightEntries()
         {
-            return _arthwindsContext.WeightEntries.OrderBy(e => e.CreatedDate);
+            try
+            {
+                return _arthwindsContext.WeightEntries.OrderBy(e => e.CreatedDate);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to get all products: {ex}");
+                return null;
+            }
         }
 
 
